7e10c5e8767f2b523f30128249a20c4c60da41a9
cstj-physchim <cstj.physchim@gmail.com>
2026-08-20 19:40:34 +0200

    Update levels.py

--- levels.py
+++ levels.py
@@ -62,28 +62,71 @@ LEVELS = {
         "theme": "Électricité",
         "order": ["A1","A15","A4","A5","A6","A12","A16","A9","A8","A10","A11","A7","A2","A13","A14","A3"],
         "dominos": {
-            "A1": ("elec_violet_A1_montage", "img:elec_violet_A1_schema"),
-            "A15": ("elec_violet_A15_montage", "img:elec_violet_A15_schema"),
-            "A4": ("elec_violet_A4_montage", "img:elec_violet_A4_schema"),
-            "A5": ("elec_violet_A5_montage", "img:elec_violet_A5_schema"),
-            "A6": ("elec_violet_A6_montage", "img:elec_violet_A6_schema"),
-            "A12": ("elec_violet_A12_montage", "img:elec_violet_A12_schema"),
-            "A16": ("elec_violet_A16_montage", "img:elec_violet_A16_schema"),
-            "A9": ("elec_violet_A9_montage", "img:elec_violet_A9_schema"),
-            "A8": ("elec_violet_A8_montage", "img:elec_violet_A8_schema"),
-            "A10": ("elec_violet_A10_montage", "img:elec_violet_A10_schema"),
-            "A11": ("elec_violet_A11_montage", "img:elec_violet_A11_schema"),
-            "A7": ("elec_violet_A7_montage", "img:elec_violet_A7_schema"),
-            "A2": ("elec_violet_A2_montage", "img:elec_violet_A2_schema"),
-            "A13": ("elec_violet_A13_montage", "img:elec_violet_A13_schema"),
-            "A14": ("elec_violet_A14_montage", "img:elec_violet_A14_schema"),
-            "A3": ("elec_violet_A3_montage", "img:elec_violet_A3_schema"),
+            "A1": "violet_1.png", "A2": "violet_2.png", "A3": "violet_3.png", "A4": "violet_4.png",
+            "A5": "violet_5.png", "A6": "violet_6.png", "A7": "violet_7.png", "A8": "violet_8.png",
+            "A9": "violet_9.png", "A10": "violet_10.png", "A11": "violet_11.png", "A12": "violet_12.png",
+            "A13": "violet_13.png", "A14": "violet_14.png", "A15": "violet_15.png", "A16": "violet_16.png",
+        },
+    },
+    "Jaune": {
+        "emoji": "🟡",
+        "theme": "Électricité",
+        "order": ["A1","A10","A16","A12","A6","A8","A7","A15","A11","A5","A3","A2","A13","A14","A4","A9"],
+        "dominos": {
+            "A1": "jaune_1.png", "A2": "jaune_2.png", "A3": "jaune_3.png", "A4": "jaune_4.png",
+            "A5": "jaune_5.png", "A6": "jaune_6.png", "A7": "jaune_7.png", "A8": "jaune_8.png",
+            "A9": "jaune_9.png", "A10": "jaune_10.png", "A11": "jaune_11.png", "A12": "jaune_12.png",
+            "A13": "jaune_13.png", "A14": "jaune_14.png", "A15": "jaune_15.png", "A16": "jaune_16.png",
+        },
+    },
+    "Vert": {
+        "emoji": "🟢",
+        "theme": "Électricité",
+        "order": ["A1","A11","A7","A9","A6","A4","A13","A10","A5","A2","A16","A12","A3","A15","A14","A8"],
+        "dominos": {
+            "A1": "vert_1.png", "A2": "vert_2.png", "A3": "vert_3.png", "A4": "vert_4.png",
+            "A5": "vert_5.png", "A6": "vert_6.png", "A7": "vert_7.png", "A8": "vert_8.png",
+            "A9": "vert_9.png", "A10": "vert_10.png", "A11": "vert_11.png", "A12": "vert_12.png",
+            "A13": "vert_13.png", "A14": "vert_14.png", "A15": "vert_15.png", "A16": "vert_16.png",
+        },
+    },
+    "Bleu": {
+        "emoji": "🔵",
+        "theme": "Électricité",
+        "order": ["A1","A6","A13","A2","A11","A3","A12","A4","A15","A9","A7","A5","A10","A16","A14","A8"],
+        "dominos": {
+            "A1": "bleu_1.png", "A2": "bleu_2.png", "A3": "bleu_3.png", "A4": "bleu_4.png",
+            "A5": "bleu_5.png", "A6": "bleu_6.png", "A7": "bleu_7.png", "A8": "bleu_8.png",
+            "A9": "bleu_9.png", "A10": "bleu_10.png", "A11": "bleu_11.png", "A12": "bleu_12.png",
+            "A13": "bleu_13.png", "A14": "bleu_14.png", "A15": "bleu_15.png", "A16": "bleu_16.png",
+        },
+    },
+    "Orange": {
+        "emoji": "🟠",
+        "theme": "Électricité",
+        "order": ["A1","A5","A13","A9","A4","A10","A16","A11","A14","A12","A2","A6","A3","A8","A7","A15"],
+        "dominos": {
+            "A1": "orange_1.png", "A2": "orange_2.png", "A3": "orange_3.png", "A4": "orange_4.png",
+            "A5": "orange_5.png", "A6": "orange_6.png", "A7": "orange_7.png", "A8": "orange_8.png",
+            "A9": "orange_9.png", "A10": "orange_10.png", "A11": "orange_11.png", "A12": "orange_12.png",
+            "A13": "orange_13.png", "A14": "orange_14.png", "A15": "orange_15.png", "A16": "orange_16.png",
+        },
+    },
+    "Rouge": {
+        "emoji": "🔴",
+        "theme": "Électricité",
+        "order": ["A1","A2","A13","A4","A14","A15","A5","A7","A9","A12","A11","A16","A3","A8","A6","A10"],
+        "dominos": {
+            "A1": "rouge_1.png", "A2": "rouge_2.png", "A3": "rouge_3.png", "A4": "rouge_4.png",
+            "A5": "rouge_5.png", "A6": "rouge_6.png", "A7": "rouge_7.png", "A8": "rouge_8.png",
+            "A9": "rouge_9.png", "A10": "rouge_10.png", "A11": "rouge_11.png", "A12": "rouge_12.png",
+            "A13": "rouge_13.png", "A14": "rouge_14.png", "A15": "rouge_15.png", "A16": "rouge_16.png",
         },
     },
 }
 
 MOLECULE_LEVEL_NAMES = ["Facile", "Moyen", "Difficile", "Très difficile"]
-ELECTRICITY_LEVEL_NAMES = ["Violet"]
+ELECTRICITY_LEVEL_NAMES = ["Violet", "Jaune", "Vert", "Bleu", "Orange", "Rouge"]
 
 # Compatibilité avec le code plus ancien
 LEVEL_NAMES = MOLECULE_LEVEL_NAMES
